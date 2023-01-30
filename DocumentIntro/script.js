ace44bdb07aa1debac24d45a52903f60b78b3741
davigomessales <davi.gomes.sales@gmail.com>
2023-01-30 20:13:19 -0300

    update files

---
+++ DocumentIntro/script.js
@@ -0,0 +1,7 @@
+/**
+ * Use a propriedade .innerHTML para modificar o conteúdo
+ * do elemento <body> para exibir um cabeçalho <h1> com o
+ * texto 'This is now the heading of the body element'.
+Observe como o conteúdo anterior dentro do elemento <body> foi substituído!
+ */
+
