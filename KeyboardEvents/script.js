fc1ca641147e75a4e9a92c9a768af9574051dc9f
davigomessales <davi.gomes.sales@gmail.com>
2023-02-03 18:55:17 -0300

    add keyboard instructions

--- KeyboardEvents/script.js
+++ KeyboardEvents/script.js
@@ -1,3 +1,24 @@
+/**
+ * 1. Agora é hora de criar um jogo! Programe este código para driblar
+ * a bola na plataforma usando qualquer tecla do teclado. Quando um usuário
+ * pressiona uma tecla, ele deve levantar a bola. Quando o usuário soltar a tecla,
+ * a bola deve cair. 
+Primeiro, crie uma função chamada up() que aumentará a posição inferior da bola
+para '250px'.
+2. Em seguida, crie uma função chamada down() que diminuirá a posição
+inferior da bola para '50px'.
+
+3. Crie uma propriedade do manipulador de eventos que execute a função up()
+quando um evento keydown for acionado no objeto do documento ou
+em qualquer lugar no DOM, como o destino do evento.
+
+4. Crie uma propriedade do manipulador de eventos que executa a função down() 
+quando um evento keyup é acionado no documento.
+
+Execute seu código e brinque com os eventos do teclado para fazer a bola quicar
+na plataforma. Você pode tentar teclas como a barra de espaço, teclas de letras
+ou teclas numéricas!
+ */
 let ball = document.getElementById('float-circle');
 
 // Write your code below
